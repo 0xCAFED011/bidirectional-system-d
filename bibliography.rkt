e81f23d0b62102189bfee89d3371ede054e833e8
Viv Hammerschmidt <viv@world-wide.website>
2025-06-27 05:54:50 -0600

    add entries to bibliography

--- bibliography.rkt
+++ bibliography.rkt
@@ -6,7 +6,8 @@
          bidi-system-l
          basics-bidirectional
          bidirectional-typing
-         quantitative)
+         quantitative
+         copatterns)
 
 
 (define comp/classical
@@ -18,7 +19,8 @@
                                 #:volume 16
                                 #:number 3
                                 #:pages '("13:1" "13:57"))
-   #:url "https://arxiv.org/pdf/1907.13227"))
+   #:url "https://arxiv.org/pdf/1907.13227"
+   #:doi "https://doi.org/10.23638/LMCS-16(3:13)2020"))
 
 (define bidi-system-l
   (make-bib
@@ -55,3 +57,23 @@
    #:location (proceedings-location "LICS '18: 33rd Anual ACM/IEEE Symposium on Logic in Computer Science"
                                     #:pages '(56 65))
    #:doi "https://doi.org/10.1145/3209108.3209189"))
+
+(define copatterns
+  (make-bib
+   #:title "Copatterns: Programming Infinite Structures by Observations"
+   #:author (authors "Andreas Abel" "Brigitte Pientka" "David Thibodeau" "Anton Setzer")
+   #:date 2013
+   #:location (journal-location "ACM SIGPLAN Notices"
+                                #:volume 48
+                                #:number 1
+                                #:pages '(27 38))
+   #:doi "https://doi.org/10.1145/2480359.2429075"))
+
+(define cocontext
+  (make-bib
+   #:title "A Co-contextual Formulation of Type Rules and Its Application to Incremental Type Checking"
+   #:author (authors "Sebastian Erdweg" "Oliver Bračevac" "Edlira Kuci" "Matthias Krebs" "Mira Mezini")
+   #:date 2015
+   #:location (proceedings-location "OOPSLA 2015: Proceedings of the 2015 ACM SIGPLAN International Conference on Object-Oriented Programming, Systems, Languages, and Applications"
+                                    #:pages '(880 897))
+   #:doi "https://doi.org/10.1145/2814270.2814277"))
